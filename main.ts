4cb9a2bfdf13ccb264a0c85134d00ac4176c79ee
robinfaury <robinfaury@gmail.com>
2023-04-29 22:16:46 +0200

    Add the background

--- main.ts
+++ main.ts
@@ -3,8 +3,28 @@ import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js';
 
+function CreateRenderer() {
+  var canvas = document.createElement('canvas');
+  var context = canvas.getContext('webgl2');
+  if (context) {
+      return new THREE.WebGLRenderer({
+          alpha: true,
+          antialias: true,
+          canvas: canvas,
+          context: context
+      });
+  } else {
+      return new THREE.WebGLRenderer({
+          alpha: true,
+          antialias: true,
+      });
+  }
+}
+
+const renderer:THREE.WebGLRenderer = CreateRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+
 // Environment
-const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.toneMappingExposure = 1;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -23,7 +43,6 @@ let time = 0;
 
 // Objects
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x555555);
 scene.add(new THREE.AmbientLight(0xffffff, 1));
 const spot = new THREE.PointLight(0xffffff, 1);
 scene.add(spot);
@@ -72,6 +91,38 @@ new RGBELoader()
     scene.environment = texture;
   });
 
+const scene_background = new THREE.Scene();
+var uniforms_background = {
+  "time": {value: 0.5},
+  "center": {type: "v2", value: new THREE.Vector2(1.0, 0.0)},
+};
+var sky_shader = new THREE.ShaderMaterial({
+  uniforms : uniforms_background,
+  vertexShader : `
+    out vec2 out_uv;
+    void main() {
+        gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+        out_uv = vec2(position.x+1.0, position.y+1.0);
+    }
+    `,
+  fragmentShader : `
+    precision highp float;
+    in vec2 out_uv;
+    out vec4 output_color;
+    uniform float time;
+    uniform vec2 center;
+    void main() {
+      highp float dist = length(out_uv-center)*0.56;
+      output_color = vec4(mix(vec3(0.682, 0.886, 0.973), vec3(0.063, 0.153, 0.239), dist), 1.0);
+    }
+  `,
+  glslVersion: THREE.GLSL3,
+});
+sky_shader.depthWrite = false;
+var background_plane = new THREE.PlaneGeometry(2, 2);
+var background_mesh = new THREE.Mesh(background_plane, sky_shader);
+scene_background.add(background_mesh);
+
 // Events
 window.addEventListener("resize", onWindowResize, false);
 function onWindowResize() {
@@ -88,6 +139,10 @@ function renderLoop() {
   spot.position.y = 2 * Math.sin(time);
 
   controls.update();
+
+  renderer.autoClear = false;
+  renderer.clear();
+  renderer.render(scene_background, camera);
   renderer.render(scene, camera);
 }
 renderLoop();
