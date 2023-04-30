6d2a8554ff8f5cd0b5bc6bc01137be5635395477
robinfaury <robinfaury@gmail.com>
2023-04-30 23:22:35 +0200

    try a fix

--- utils.ts
+++ utils.ts
@@ -23,20 +23,21 @@ function fract(input:number) {
 }
 
 export function Noise3D(position:THREE.Vector3) {
-  var a = new THREE.Vector3(Math.floor(position.x), Math.floor(position.y), Math.floor(position.z));
-  var d = new THREE.Vector3(position.x-a.x, position.y-a.y, position.z-a.z);
+  var p = position.clone();
+  const a = new THREE.Vector3(Math.floor(p.x), Math.floor(p.y), Math.floor(p.z));
+  var d = new THREE.Vector3(p.x-a.x, p.y-a.y, p.z-a.z);
   d.x = d.x*d.x*(3.0-2.0*d.x);  
   d.y = d.y*d.y*(3.0-2.0*d.y);  
   d.z = d.z*d.z*(3.0-2.0*d.z);
-  var b = new THREE.Vector4(a.x, a.x+1.0, a.y, a.y+1.0);
-  var k1 = perm(new THREE.Vector4(b.x, b.y, b.x, b.y));
-  var k2 = perm(new THREE.Vector4(k1.x+b.z, k1.y+b.z, k1.x+b.w, k1.y+b.w));
-  var c = new THREE.Vector4(k2.x+a.z, k2.y+a.z, k2.z+a.z, k2.w+a.z);
-  var k3 = perm(c);
-  var k4 = perm(new THREE.Vector4(c.x+1.0, c.y+1.0, c.z+1.0, c.w+1.0));
-  var o1 = new THREE.Vector4(fract(k3.x*(1.0/41.0)), fract(k3.y*(1.0/41.0)), fract(k3.z*(1.0/41.0)), fract(k3.w*(1.0/41.0)));
-  var o2 = new THREE.Vector4(fract(k4.x*(1.0/41.0)), fract(k4.y*(1.0/41.0)), fract(k4.z*(1.0/41.0)), fract(k4.w*(1.0/41.0)));
-  var o3 = new THREE.Vector4(o2.x*d.z+o1.x*(1.0-d.z), o2.y*d.z+o1.y*(1.0-d.z), o2.z*d.z+o1.z*(1.0-d.z), o2.w*d.z+o1.w*(1.0-d.z));
-  var o4 = new THREE.Vector2(o3.y*d.x+o3.x*(1.0-d.x), o3.w*d.x+o3.z*(1.0-d.x));
+  const b = new THREE.Vector4(a.x, a.x+1.0, a.y, a.y+1.0);
+  const k1 = perm(new THREE.Vector4(b.x, b.y, b.x, b.y));
+  const k2 = perm(new THREE.Vector4(k1.x+b.z, k1.y+b.z, k1.x+b.w, k1.y+b.w));
+  const c = new THREE.Vector4(k2.x+a.z, k2.y+a.z, k2.z+a.z, k2.w+a.z);
+  const k3 = perm(c);
+  const k4 = perm(new THREE.Vector4(c.x+1.0, c.y+1.0, c.z+1.0, c.w+1.0));
+  const o1 = new THREE.Vector4(fract(k3.x*(1.0/41.0)), fract(k3.y*(1.0/41.0)), fract(k3.z*(1.0/41.0)), fract(k3.w*(1.0/41.0)));
+  const o2 = new THREE.Vector4(fract(k4.x*(1.0/41.0)), fract(k4.y*(1.0/41.0)), fract(k4.z*(1.0/41.0)), fract(k4.w*(1.0/41.0)));
+  const o3 = new THREE.Vector4(o2.x*d.z+o1.x*(1.0-d.z), o2.y*d.z+o1.y*(1.0-d.z), o2.z*d.z+o1.z*(1.0-d.z), o2.w*d.z+o1.w*(1.0-d.z));
+  const o4 = new THREE.Vector2(o3.y*d.x+o3.x*(1.0-d.x), o3.w*d.x+o3.z*(1.0-d.x));
   return o4.y*d.y+o4.x*(1.0-d.y);
 }
